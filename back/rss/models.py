75911af52fa3beacf37a835bcc1ee262ffa7b2bb
Taras Kulyk <taras.kulyk1@gmail.com>
2022-03-23 19:44:26 +0200

    Back. Add models and migrations for jobs

--- back/rss/models.py
+++ back/rss/models.py
@@ -6,23 +6,6 @@ from django.db import models
 #     name = models.CharField(max_length=255, blank=False)
 #     created = models.DateTimeField(auto_now_add=True)
 
-# class Jobs(models.Model):
-# #     Country
-# # content
-# # published
-#     title = models.CharField(max_length=255, blank=True, default='')
-#     content = models.TextField(blank=True)
-#     upwork_id = models.CharField(max_length=255, blank=True, default='')
-#     published = models.DateTimeField(blank=True)
-#     country = models.ForeignKey('rss.Country', related_name='country', on_delete=models.SET_NULL)
-#     created = models.DateTimeField(auto_now_add=True)
-
-#     # rss = models.ForeignKey('user_rss.Country', related_name='country', on_delete=models.SET_NULL)
-#     # topic = models.CharField(choices=TOPIC_CHOICES, null=True, max_length=100)
-#     # q = models.CharField(blank=True, max_length=100)
-#     # active = models.BooleanField(default=False)
-#     # user = models.ForeignKey(
-#     #     'auth.User', related_name='rss', on_delete=models.CASCADE)
 
 TYPE_CHOICES = [
     ('topic', 'topic'),
@@ -33,6 +16,7 @@ TOPIC_CHOICES = [
     ('most-recent', 'most-recent'),
     ('best-matches', 'best-matches'),
 ]
+
 
 class Upwork(models.Model):
     created = models.DateTimeField(auto_now_add=True)
@@ -56,3 +40,20 @@ class Secret(models.Model):
 
     user = models.OneToOneField(
         'auth.User', related_name='rss_secret', on_delete=models.CASCADE)
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Jobs(models.Model):
+    title = models.CharField(max_length=255, null=True, default='')
+    content = models.TextField(null=True)
+    upwork_id = models.CharField(max_length=255, null=True, default='')
+    published = models.DateTimeField(null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    country = models.ForeignKey(
+        'rss.Country', related_name='country', null=True, on_delete=models.SET_NULL)
+    rss = models.ForeignKey(
+        'rss.Upwork', related_name='rss', null=True, on_delete=models.SET_NULL)
