0b86d635e2a98cd25d64dd9c5594823c36d7fe6c
Taras Kulyk <taras.kulyk1@gmail.com>
2022-03-23 16:06:31 +0200

    Front. Add new menu items

--- front/src/components/Sidebar/Sidebar.vue
+++ front/src/components/Sidebar/Sidebar.vue
@@ -75,6 +75,38 @@
         <h6
           class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
         >
+          Upwork
+        </h6>
+        <!-- Navigation -->
+
+        <ul class="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
+          <li class="items-center">
+            <router-link
+              class="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
+              :to="{'name':'admin.rss.upwork.index'}"
+            >
+              <i class="fas fa-rss text-blueGray-300 mr-2 text-sm"></i>
+              My Upwork RSS
+            </router-link>
+          </li>
+          
+          <li class="items-center">
+            <router-link
+              class="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
+              :to="{'name':'admin.rss.upwork.index'}"
+            >
+              <i class="fas fa-fingerprint text-blueGray-300 mr-2 text-sm"></i>
+              My Secrets
+            </router-link>
+          </li>
+        </ul>
+
+        <!-- Divider -->
+        <hr class="my-4 md:min-w-full" />
+        <!-- Heading -->
+        <h6
+          class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+        >
           Admin Layout Pages
         </h6>
         <!-- Navigation -->
