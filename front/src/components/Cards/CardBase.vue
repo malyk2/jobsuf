5fcf14ae4110ae8d40fa2431c071277c5f6a57ca
Taras Kulyk <taras.kulyk1@gmail.com>
2022-03-23 16:36:00 +0200

    Front. Clear card header slot

--- front/src/components/Cards/CardBase.vue
+++ front/src/components/Cards/CardBase.vue
@@ -17,30 +17,6 @@
     <div class="rounded-t bg-white mb-0 px-6 py-6">
       <div class="text-center flex justify-between">
         <slot name="header">
-        <button
-          class="
-            bg-emerald-500
-            text-white
-            active:bg-emerald-600
-            font-bold
-            uppercase
-            text-xs
-            px-4
-            py-2
-            rounded
-            shadow
-            hover:shadow-md
-            outline-none
-            focus:outline-none
-            mr-1
-            ease-linear
-            transition-all
-            duration-150
-          "
-          type="button"
-        >
-          Settings
-        </button>
         </slot>
       </div>
     </div>
