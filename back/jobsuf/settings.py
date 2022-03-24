6ba7c61d82e76cc28f1bc125e511ae12c19e8108
Taras Kulyk <taras.kulyk1@gmail.com>
2022-03-24 11:22:39 +0200

    Back. Add log sql queries

--- back/jobsuf/settings.py
+++ back/jobsuf/settings.py
@@ -167,3 +167,27 @@ CRONJOBS = [
 ]
 # python manage.py crontab add
 # python manage.py crontab show
+
+LOG_QUERY = env('LOG_QUERY', default=False)
+if LOG_QUERY: 
+    LOGGING = {
+        'version': 1,
+        'filters': {
+            'require_debug_true': {
+                '()': 'django.utils.log.RequireDebugTrue',
+            }
+        },
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'filters': ['require_debug_true'],
+                'class': 'logging.StreamHandler',
+            }
+        },
+        'loggers': {
+            'django.db.backends': {
+                'level': 'DEBUG',
+                'handlers': ['console'],
+            }
+        }
+    }
