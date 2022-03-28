34ef1c972eb9ff5cdc02c280eb7cebcc12b1bba4
Taras Kulyk <taras.kulyk1@gmail.com>
2022-03-28 22:46:53 +0300

    IP 3

--- back/jobsuf/settings.py
+++ back/jobsuf/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY', default='some-secret-key')
 CSRF_COOKIE_DOMAIN=env('CSRF_COOKIE_DOMAIN', default='localhost')
-SESSION_COOKIE_SAMESITE=env('SESSION_COOKIE_SAMESITE', default='Lax')
-CSRF_COOKIE_SAMESITE=env('CSRF_COOKIE_SAMESITE', default='Lax')
+# SESSION_COOKIE_SAMESITE=env('SESSION_COOKIE_SAMESITE', default='Lax')
+# CSRF_COOKIE_SAMESITE=env('CSRF_COOKIE_SAMESITE', default='Lax')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
@@ -36,7 +36,6 @@ DEBUG = env('DEBUG', default=False)
 ALLOWED_HOSTS = [
     'localhost',
     'api.jobsuf.da',
-    'jobsuf.da',
 ]
 
 
