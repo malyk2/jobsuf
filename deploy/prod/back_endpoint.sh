396aab561a2f48b710b66e89c57493a46b19fdbd
Taras Kulyk <taras.kulyk1@gmail.com>
2022-03-28 23:26:55 +0300

    IP 4

--- deploy/prod/back_endpoint.sh
+++ deploy/prod/back_endpoint.sh
@@ -1,4 +1,5 @@
 python /var/www/back/manage.py migrate \
     && python /var/www/back/manage.py crontab add \
+    && printenv > /var/www/back/jobsuf/.env #need for cron jobs
     && service supervisor start \
     && uwsgi --ini /var/www/uwsgi.ini
