d41706e3ba793fc0a812407f23f9fa6e933fd529
Taras Kulyk <taras.kulyk1@gmail.com>
2022-03-29 17:14:21 +0300

    Add --no-input

--- deploy/prod/back_endpoint.sh
+++ deploy/prod/back_endpoint.sh
@@ -1,6 +1,6 @@
 python /var/www/back/manage.py migrate \
     && python /var/www/back/manage.py crontab add \
-    && python /var/www/back/manage.py collectstatic \
+    && python /var/www/back/manage.py collectstatic --no-input \
     && printenv > /var/www/back/jobsuf/.env \
     && service supervisor start \
     && uwsgi --ini /var/www/uwsgi.ini
